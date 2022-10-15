60ba6e00dab2708cd5b1715a7c55eeec4c574b5a
Alex-Nik-Vel <alex.nik.vel@gmail.com>
2022-10-15 09:28:46 +0300

    begin h-w @ fix json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './css/styles.css';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import debounce from 'lodash.debounce';
+// const DEBOUNCE_DELAY = 300;
 
-const DEBOUNCE_DELAY = 300;
+
+// const refs = {
+//     searchInput: document.querySelector('search-box'),
+//     countryList: document.querySelector('.country-list'),
+//     countryInfo: document.querySelector('.country-info'),
+// };
+
+// refs.searchInput.addEventListener('input',
+//     debounce(onInputCountry, DEBOUNCE_DELAY));
+
+const BASE_URL = 'https://restcountries.com/#api-endpoints-v3-name';
+const FITER = '?fields=name,capital,population,flags,languages';
+const resp = fetch(`${BASE_URL}${name}${FITER}`);
+resp.then(response => {
+    if (!response.ok) {
+        throw new Error()
+    }
+    console.log(response);
+    return response.json();
+});
